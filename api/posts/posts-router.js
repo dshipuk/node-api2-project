5a2e5141f2a6dad147633bcaad5e0cc6639c2338
unknown <dshipuk@yahoo.com>
2022-05-10 13:37:13 -0700

    o

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -22,7 +22,7 @@ router.get("/:id", async (req,res) => {
     try {
         const post = await Post.findById(req.params.id)
         if(!post) {
-            res.status(400).json({
+            res.status(404).json({
                 message: "The post with the specified ID does not exist"
             })
         } else {
@@ -61,12 +61,59 @@ router.post("/", (req,res) => {
     }
 })
 
-router.delete("/:id", (req,res) => {
-
+router.delete("/:id", async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack
+        })
+    }
 })
 
 router.put("/:id", (req,res) => {
-
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.findById(req.params.id)
+            .then(result => {
+                if (!result) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                    })
+                } else {
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(result => {
+                if(result) {
+                    return Post.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                res.json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The post information could not be modified",
+                    err: err.message,
+                    stack: err.stack
+                })
+            })
+    }
 })
 
 router.get("/:id/messages", (req,res) => {
